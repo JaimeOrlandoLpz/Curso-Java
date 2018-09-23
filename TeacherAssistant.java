01a87dc5da59fb95b48e5c8d8faf0d2098e19c00
Jaime Orlando Lopez Ramos <30807749+JaimeOrlandoLpz@users.noreply.github.com>
2018-09-23 15:33:08 -0500

    Checa Corrección
    Para: Siham

--- TeacherAssistant.java
+++ TeacherAssistant.java
@@ -1,17 +1,22 @@
-package mx.itesm.other;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividadesalumnos;
+//Actividad Siham
 
 import java.util.LinkedList;
-import mx.itesm.other.*;
-import mx.itesm.util.*;
 
-public class TeacherAssistant {
-
-	public static LinkedList<MathStudent> findSystemsStudents (LinkedList<MathStudent> studentList){
+ public class TeacherAssistant {
+ 	public static LinkedList<MathStudent> findSystemsStudents (LinkedList<MathStudent> studentList){
 		
 		LinkedList<MathStudent> names = new LinkedList<MathStudent>();
 		
 		for (int i = 0; i < studentList.size(); i++) {
-			names.add(studentList.get(i));
+			if(studentList.get(i).getCareer().toUpperCase().equals("ISC")){
+                            names.add(studentList.get(i));
+                        }
 		}
 		
 		return names;
@@ -44,23 +49,24 @@ public class TeacherAssistant {
 		
 		return best;
 	}
-
-	public static void main(String[] args) {
-		
+ 	public static void main(String[] args) {
+		MathStudent milly = new MathStudent("Milly", "IMI", 1, 93);
 		MathStudent albert = new MathStudent("Albert", "isC", 2, 93);
 		MathStudent lizz = new MathStudent("Lizz", "IBT", 4, 93);
 		MathStudent luigi = new MathStudent("Luigi", "isc", 3, 87);
 		MathStudent mandy = new MathStudent("Mandy", "IMI", 1, 91);
+                MathStudent paola = new MathStudent("Paola", "IBT", 5, 93);
 		LinkedList<MathStudent> students = new LinkedList<MathStudent>();
 		students.add(albert);
 		students.add(lizz);
 		students.add(luigi);
 		students.add(mandy);
+                students.add(paola);
+                students.add(milly);
 		LinkedList<MathStudent> sorted = findSystemsStudents(students);
 		LinkedList<MathStudent> bestStudents = bestStudent(students);
 		System.out.println(sorted.toString());
 		System.out.println(bestStudents.toString());
 		
 	}
-
-}
+ }
